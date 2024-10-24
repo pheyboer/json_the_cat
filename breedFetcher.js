94b9efde7b6c482cd169d809bfc0df44fb24979d
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-24 18:30:43 -0400

    structuring breedfetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,9 +3,14 @@ const needle = require('needle');
 // API endpoint to search breed information
 const apiUrl = 'https://api.thecatapi.com/v1/breeds/search';
 
+//get breed name from command line arugments
+const breedName = process.argv[2];
+
 needle.get(apiUrl, (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the homepage.
   console.log(typeof body);
+
+  
 });
