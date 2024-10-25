b2ace529038ce28bbe97082ae50e797b901a45fc
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-25 11:18:17 -0400

    updating index.js for refactoring

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const { fetchBreedDescription } = require('./breedFetcher');
 //get breed name from command line arugments
 const breedName = process.argv[2];
 
-fetchBreedDescription(breedName, (error, description) => {
+fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log("Error fetch details:", error);
   } else {
-  console.log(desc);
+    console.log(desc);
   }
 });
