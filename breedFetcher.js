920bacba5743b78f0a0f2d57a99c98e5646b3db6
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-25 10:46:57 -0400

    updating breedFetcher.js with error responses

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,14 +6,18 @@ const apiUrl = 'https://api.thecatapi.com/v1/breeds/search';
 //get breed name from command line arugments
 const breedName = process.argv[2];
 
-
+//search for breed name as part of the query parameter
 needle.get(`${apiUrl}?q=${breedName}`, (error, response, body) => {
-  //search for breed name as part of the query parameter
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the homepage.
-  console.log(typeof body);
-
+  if (error) {
+    console.log('error:', error); // Print the error if one occurred
+    if (response) {
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body); // Print the HTML for the homepage.
+    //console.log(typeof body);
+    }
+    return;
+  }
+  
   if (Array.isArray(body) && body.length > 0) {
     const firstBreed = body[0];
     console.log(`The first breed is: ${firstBreed.name}: ${firstBreed.description}`);
