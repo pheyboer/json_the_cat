f683b3fda3e8607db7fb12fac9f870cbd567ca23
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-25 11:18:03 -0400

    updating breedFetcher for refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,25 +3,28 @@ const needle = require('needle');
 // API endpoint to search breed information
 const apiUrl = 'https://api.thecatapi.com/v1/breeds/search';
 
-//get breed name from command line arugments
-const breedName = process.argv[2];
+
 
 //search for breed name as part of the query parameter
-needle.get(`${apiUrl}?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-    if (response) {
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //console.log('body:', body); // Print the HTML for the homepage.
-    //console.log(typeof body);
+const fetchBreedDescription = function(breedName, callback) {
+  needle.get(`${apiUrl}?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      console.log('error:', error); // Print the error if one occurred
+      if (response) {
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        //console.log('body:', body); // Print the HTML for the homepage.
+        //console.log(typeof body);
+      }
+      return;
     }
-    return;
-  }
   
-  if (Array.isArray(body) && body.length > 0) {
-    const firstBreed = body[0];
-    console.log(`The first breed is: ${firstBreed.name}: ${firstBreed.description}`);
-  } else {
-    console.log(`Breed not found: "${breedName}"`);
-  }
-});
+    if (Array.isArray(body) && body.length > 0) {
+      const firstBreed = body[0];
+      console.log(`The first breed is: ${firstBreed.name}: ${firstBreed.description}`);
+    } else {
+      console.log(`Breed not found: "${breedName}"`);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
