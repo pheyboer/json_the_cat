58cf5d7f49c6c1ece5fd983ef4f75a96aaf4e94b
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-25 11:36:53 -0400

    updating and refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -9,20 +9,23 @@ const apiUrl = 'https://api.thecatapi.com/v1/breeds/search';
 const fetchBreedDescription = function(breedName, callback) {
   needle.get(`${apiUrl}?q=${breedName}`, (error, response, body) => {
     if (error) {
-      console.log('error:', error); // Print the error if one occurred
-      if (response) {
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        //console.log('body:', body); // Print the HTML for the homepage.
-        //console.log(typeof body);
-      }
+      callback(`error: ${error}`, null);
       return;
     }
+    //console.log('error:', error); // Print the error if one occurred
+    // if (response) {
+    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body); // Print the HTML for the homepage.
+    //console.log(typeof body);
+    // }
+    // return;
+    // }
   
     if (Array.isArray(body) && body.length > 0) {
       const firstBreed = body[0];
-      console.log(`The first breed is: ${firstBreed.name}: ${firstBreed.description}`);
+      callback(null, `${firstBreed.description}`);
     } else {
-      console.log(`Breed not found: "${breedName}"`);
+      callback(`Breed not found: "${breedName}"`, null);
     }
   });
 };
